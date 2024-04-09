6101de8217a9930edb9ef8f1349d41c533d8be51
boxfriend <cawills394@gmail.com>
2024-04-09 18:54:52 -0400

    finished implementation of save data generator

--- MHC-Generators/SaveDataGenerator.cs
+++ MHC-Generators/SaveDataGenerator.cs
@@ -3,53 +3,150 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
 namespace MHC_Generators
 {
     [Generator]
-    internal class SaveDataGenerator : ISourceGenerator
+    internal sealed class SaveDataGenerator : ISourceGenerator
     {
+        private const string _saveAttribute = "Save";
+
+        private const string _attribute = @"
+using System;
+namespace Boxfriend.Generators
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class SaveDataAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class SaveAttribute : Attribute { }
+}
+";
+
         public void Execute (GeneratorExecutionContext context)
         {
             var saveClasses = ((SaveDataSyntaxReceiver)context.SyntaxReceiver)?.SaveClasses;
-            if (saveClasses is null || saveClasses.Count < 1)
+                
+            if (saveClasses is null)
                 return;
 
             var dataBuilder = new StringBuilder();
-            foreach(var saveClass in saveClasses)
+            foreach (var saveClass in saveClasses)
             {
+                var className = saveClass.Identifier.ToString();
                 var members = GetSaveMembers(saveClass);
-                BuildSaveStruct($"{saveClass.Identifier}SaveData", members, dataBuilder);
-                context.AddSource($"{saveClass.Identifier}SaveData", SourceText.From(dataBuilder.ToString(), Encoding.UTF8));
-                dataBuilder.Clear();
-                
-                //TODO: Check if class is partial before proceeding
-
-                BuildSaveMethod(saveClass.Identifier.ToString(), members, dataBuilder);
-                context.AddSource($"{saveClass.Identifier}-ToData", SourceText.From(dataBuilder.ToString(), Encoding.UTF8));
+                BuildSaveStruct(className, members, dataBuilder);
+                context.AddSource($"{saveClass.Identifier}SaveData.g.cs", SourceText.From(dataBuilder.ToString(), Encoding.UTF8));
                 dataBuilder.Clear();
 
-                BuildLoadMethod(saveClass.Identifier.ToString(), members, dataBuilder);
-                context.AddSource($"{saveClass.Identifier}-FromData", SourceText.From(dataBuilder.ToString(), Encoding.UTF8));
+                if (!saveClass.Modifiers.Any(x => x.IsKind(SyntaxKind.PartialKeyword)))
+                    continue;
+
+                BuildSaveMethod(className, members, dataBuilder);
+                context.AddSource($"{saveClass.Identifier}.ToData.g.cs", SourceText.From(dataBuilder.ToString(), Encoding.UTF8));
+                dataBuilder.Clear();
+
+                BuildLoadMethod(className, members, dataBuilder);
+                context.AddSource($"{saveClass.Identifier}.FromData.g.cs", SourceText.From(dataBuilder.ToString(), Encoding.UTF8));
                 dataBuilder.Clear();
             }
-
         }
         private void BuildSaveStruct (string className, IEnumerable<(string Type, string Identifier)> members, StringBuilder builder)
         {
+            builder.AppendLine($@"
+namespace Boxfriend.Generators
+{{
+    [System.Serializable]
+    public struct {className}SaveData
+    {{
+");
+            foreach(var (Type, Identifier) in members)
+            {
+                builder.AppendLine($"\t\tpublic {Type} {RemovePrefix(Identifier)};");
+            }
+            builder.AppendLine("\t}\n}");
+        }
 
+        private string RemovePrefix(string toModify)
+        {
+            var i = 0;
+            
+            while (!char.IsLetter(toModify[i]))
+                i++;
+
+            return toModify.Substring(i);
         }
 
         private void BuildSaveMethod(string  className, IEnumerable<(string Type, string Identifier)> members, StringBuilder builder)
         {
+            builder.AppendLine($"public partial class {className}\n{{");
+            var saveName = $"Boxfriend.Generators.{className}SaveData";
+            builder.AppendLine($"\tpublic {saveName} ToSaveData()\n\t{{");
+            builder.AppendLine($"\t\tvar data = new {saveName}();");
+            foreach(var (_, Identifier) in members)
+            {
+                builder.AppendLine($"\t\tdata.{RemovePrefix(Identifier)} = {Identifier};");
+            }
+            builder.AppendLine($"\t\treturn data;\n\t}}");
 
+            builder.AppendLine($@"
+    public async System.Threading.Tasks.Task<bool> TrySaveData(string path, System.Threading.CancellationToken cancellationToken)
+    {{
+        var data = ToSaveData();
+        try
+        {{
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            using var writer = new System.IO.StreamWriter(path);
+            await writer.WriteAsync(json);
+        }}
+        catch (System.Exception ex)
+        {{
+#if UNITY_2021_1_OR_NEWER
+            UnityEngine.Debug.LogException(ex);
+#endif
+            return false;
+        }}
+        return true;
+    }}
+}}
+");
         }
 
         private void BuildLoadMethod (string className, IEnumerable<(string Type, string Identifier)> members, StringBuilder builder)
         {
+            builder.AppendLine($"public partial class {className}\n{{");
+            var saveName = $"Boxfriend.Generators.{className}SaveData";
+            builder.AppendLine($"\tpublic void ApplySaveData({saveName} data)\n\t{{");
+            foreach (var (_, Identifier) in members)
+            {
+                builder.AppendLine($"\t\t{Identifier} = data.{RemovePrefix(Identifier)};");
+            }
+            builder.AppendLine("\t}");
 
+            builder.AppendLine($@"
+    public async System.Threading.Tasks.Task<bool> TryLoadData(string path, System.Threading.CancellationToken cancellationToken)
+    {{
+        try
+        {{
+            using var reader = new System.IO.StreamReader(path);
+            var data = await reader.ReadToEndAsync();
+            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<{saveName}>(data);
+            ApplySaveData(obj);
+        }}
+        catch (System.Exception ex)
+        {{
+#if UNITY_2021_1_OR_NEWER
+            UnityEngine.Debug.LogException(ex);
+#endif
+            return false;
+        }}
+        return true;
+    }}
+}}
+");
         }
 
         private IEnumerable<(string Type, string Identifier)> GetSaveMembers(ClassDeclarationSyntax classDeclaration)
@@ -74,19 +171,20 @@ namespace MHC_Generators
                 }
             }
 
-            yield return default;
-
             bool HasAttribute (MemberDeclarationSyntax mem) => mem.AttributeLists.SelectMany(x => x.Attributes)
-                .Where(x => x.Name.ToString() == "Save").Any();
+                .Where(x => x.Name.ToString() == _saveAttribute).Any();
         }
 
-        public void Initialize (GeneratorInitializationContext context) =>
+        public void Initialize (GeneratorInitializationContext context)
+        {
+            context.RegisterForPostInitialization(x => x.AddSource("SaveAttributes.g.cs", _attribute));
             context.RegisterForSyntaxNotifications(() => new SaveDataSyntaxReceiver());
+        }
     }
 
-    internal class SaveDataSyntaxReceiver : ISyntaxReceiver
+    internal sealed class SaveDataSyntaxReceiver : ISyntaxReceiver
     {
-        private static readonly string _attributeName = "";
+        private const string _attributeName = "SaveData";
 
         public List<ClassDeclarationSyntax> SaveClasses { get; } = new List<ClassDeclarationSyntax>();
 
